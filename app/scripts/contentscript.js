c8d0313f7d20ec9023bbbaa5f813d38822de21ae
Gerrard Lindsay <gerrard@pickabar.com>
2015-07-15 17:47:23 -0400

    Added c3 and jquery and used them from content script.

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,3 +1,16 @@
 'use strict';
 
+(function() {
 console.log('\'Allo \'Allo! Content script');
+
+var displayDiv;
+
+var existingChart = $('#highchart');
+
+if (existingChart) {
+	displayDiv = existingChart.after('<div id="extraCharts">gerrard</div>');
+}
+
+console.log('done now!')
+}());
+
