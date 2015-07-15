dbd0d55aa68e99e3451193cc568554b90ade6528
Gerrard Lindsay <gerrard@pickabar.com>
2015-07-15 18:08:18 -0400

    Added MyFitnessPal mock service.

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -7,8 +7,13 @@ var displayDiv;
 
 var existingChart = $('#highchart');
 
+//if the highcharts chart isn't there, we assume that we're on the wrong page (for now).
 if (existingChart) {
+
 	displayDiv = existingChart.after('<div id="extraCharts">gerrard</div>');
+
+	var data = gerrrard00.MyFitnessPalService.getWeightData();
+	console.log(data[0]);
 }
 
 console.log('done now!')
