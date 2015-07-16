e0d8fc38ce403161994554610761cfecbfc6ba1b
Gerrard Lindsay <gerrard@pickabar.com>
2015-07-16 14:04:07 -0400

    Moved data cleaning into seperate file.

--- app/scripts/MyFitnessPalDataScrubber.js
+++ app/scripts/MyFitnessPalDataScrubber.js
@@ -1,38 +1,60 @@
 var gerrard00 = gerrard00 || {};
 
-gerrard00.MyFitnessPalService = (function () {
+gerrard00.MyFitnessPalDataScrubber = (function () {
 
     'use strict';
 
-    /* global $, MyFitnessPalDataScrubber */
+    /* global $ */
 
-    //TODO: accept a callback
-    function getWeightData( sourceUrl, dataReceiver, errorReceiver ) {
+    function cleanRawData(rawData) {
+        //strip data with 0 totals. mfp returns entries for the entire date ranage,
+        //even if the user hasn't tracked anything for a particular date
+        var cleanData  =
+            rawData.filter(function(entry) {
+                return entry.total > 0;
+            })
+            //the raw json file repeats readings for every day between readings
+            .filter(function (entry, index, array) {
+                    return (index === 0) ||
+                        //TODO: we should only do this if the entry is one day before...punting for now to avoid date parsing
+                        (entry.total !== array[index - 1].total);
+                });
 
-        console.log('source url: ' + sourceUrl);
+        //mfp omits the year from dates in this year, add it back.
+        //TODO: add comments...make this a function?
+        //we start out with one year in the future, our first entry will decrement it to the current year
+        var currentYear = new Date().getFullYear();
+        var lastSeenMonth = null;
 
-        $.ajax(sourceUrl)
-          .done(function(response) {
-            console.log('mfp -> success');
+        cleanData = cleanData.reverse();
 
-            var cleanData = gerrard00.MyFitnessPalDataScrubber.cleanRawData(response.data);
+        cleanData.forEach(function(entry) {
+            var rawDate = entry.date;
 
-            dataReceiver(cleanData);
-        })
-        .fail(function( jqXHR, textStatus) {
-            console.log('mfp -> error');
-            errorReceiver('Error talking to MyFitnessPal: ' + textStatus);
-        })
-        .always(function() {
-            console.log('mfp -> complete');
+            var currentEntryMonth = rawDate.split('/')[0];
+         
+            if (currentEntryMonth !== lastSeenMonth) {
+
+                lastSeenMonth = currentEntryMonth;
+
+                //we just crossed a year boundary
+                if (currentEntryMonth === '12') {
+                    currentYear--;
+                }
+            }
+
+            var dateToUse = new Date(rawDate + '/' + currentYear);
+
+            entry.date = dateToUse;
         });
 
-        //fake data
-        //dataReceiver(cleanRawData(fakeWeightData.data));
-    }
+        cleanData = cleanData.reverse();
 
+        return cleanData;
+    }
+    
     return {
-        getWeightData : getWeightData
+        cleanRawData : cleanRawData
     };
 }());
 
