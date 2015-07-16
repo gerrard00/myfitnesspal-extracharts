bd0ad1a2924e89eb9b5bf4d56c16e64aa412995b
Gerrard Lindsay <gerrard@pickabar.com>
2015-07-15 20:16:04 -0400

    Working chart displayed.

--- app/scripts/ExponentiallySmoothedMovingAverageChart.js
+++ app/scripts/ExponentiallySmoothedMovingAverageChart.js
@@ -4,7 +4,7 @@ var gerrrard00 = gerrrard00 || {};
 gerrrard00.charts = gerrrard00.charts || {};
 
 gerrrard00.charts.ExponentiallySmoothedMovingAverageChart = (function () {
-	'use strict';
+	var exponentialMovingAverageFieldName = "exponentialMovingAverage";
 
 	//TODO: if we switch to d3, we can probably have the chart run this calculation as part of the chart generation
 	function calculateExponentialMovingAverage(currentObservation,
@@ -14,14 +14,59 @@ gerrrard00.charts.ExponentiallySmoothedMovingAverageChart = (function () {
 			//TODO: make this configurable
 			var smoothingFactor = .9;
 
-			//interpretation of hacker diet: https://www.fourmilab.ch/hackdiet/www/subsubsection1_4_1_0_8_3.html
+			//inspired by hacker diet: 
+			//https://www.fourmilab.ch/hackdiet/www/subsubsection1_4_1_0_8_3.html
 			return previousExponentialMovingAverage + 
 				((1 - smoothingFactor) * 
 					(currentObservation - previousExponentialMovingAverage));
 	}
 
 	function displayChart( displaySelector, data ) {
-		console.log(data.length);
+
+		data.forEach(function (currentEntry, index, array) {
+
+			//if we are seeing the first entry...
+			currentEntry[exponentialMovingAverageFieldName] = (index === 0)
+			//use the current observation as the ema
+		    ? currentEntry.total
+		    //otherwise, use calculate the current ema using the previous value
+		    : calculateExponentialMovingAverage(currentEntry.total, 
+		    	array[index - 1][exponentialMovingAverageFieldName]);
+
+  		});
+
+		//TODO: instead of modifying data, copy the data to viewModel
+		var viewModel = data;
+
+		//TODO: figure out the ticks on the x axis
+		//TODO: why aren't tooltips working?
+		//TODO: scatter for the real values? Default scatter has tiny points.
+		console.log(displaySelector);
+
+		var chart = c3.generate({
+			bindto: displaySelector,
+			data: {
+			    json: viewModel,
+			    keys: {
+			        x: 'date',
+			        value: ['total', exponentialMovingAverageFieldName]
+			    },
+			    types : {
+			      total : 'line'
+			    }
+			},
+			axis: {
+			    x: {
+			        type: 'timeseries'
+			    }
+			}
+		});
+
+		//TODO: add a real legend
+
+		console.log('finished generating chart');
+
+		return chart;
 	}
 
 	return { 
--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -2,7 +2,7 @@
 
 (function() {
 	console.log(gerrrard00);
-	
+
 	var loadService = gerrrard00.MyFitnessPalService;
 	var chartDisplayer = gerrrard00.charts.ExponentiallySmoothedMovingAverageChart;
 
@@ -12,17 +12,16 @@
 
 	//if the highcharts chart isn't there, we assume that we're on the wrong page (for now).
 	if (existingChart) {
-		displayDiv = existingChart.after('<div id="extraCharts">gerrard</div>');
+		displayDiv = existingChart.after('<div id="extraCharts"><label>Exponentially Smoothed Moving Average</label><div id="extraCharts_display" /></div>')[0];
 
 		var data = loadService.getWeightData(function (data) {
 		
-			chartDisplayer.displayChart(displayDiv, data);
+			//TODO: magic string 
+			chartDisplayer.displayChart('#extraCharts_display', data);
 
 		}, function (error) {
 			console.log("Error!" + error);
 		});
 	}
-
-	console.log('done now!')
 }());
 
